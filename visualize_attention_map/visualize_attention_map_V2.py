bbcb6f9fd52662f2c57c4840a5ed810b007fc86c
rentainhe <596106517@qq.com>
2021-03-14 17:04:49 +0800

    debug

--- visualize_attention_map/visualize_attention_map_V2.py
+++ visualize_attention_map/visualize_attention_map_V2.py
@@ -38,7 +38,7 @@ def visulize_attention_ratio(img_path, save_path, attention_mask, ratio=0.5, cma
         img_name = img_path.split('/')[-1].split('.')[0] + "_with_attention.jpg"
         img_with_attention_save_path = os.path.join(save_path, img_name)
         # pre-process before saving
-        print("save image to: " + save_path)
+        print("save image to: " + save_path + " as " + img_name)
         plt.axis('off')
         plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
         plt.margins(0, 0)
